b9e1890c28673ccca3c80bfe029733ca6f5b98c0
Travis Yoder <trayoda@gmail.com>
2016-05-20 15:59:23 -0700

    command works when called with different versions
    - public http urls
    - public ssh urls
    - private http urls
    - private ssh urls
    - works when there are no remotes
    - works when there are too many remotes

--- lib/brah.rb
+++ lib/brah.rb
@@ -1,5 +1,28 @@
 require "brah/version"
 
 module Brah
-  # Your code goes here...
+  class Brah
+
+    def self.run(git_remote_output)
+      new(git_remote_output)
+    end
+
+    attr_reader :command
+
+    def initialize(git_remote_output)
+      raise("No remotes found!") if git_remote_output.empty?
+      parsed_command = convert_ssh_to_http(git_remote_output)
+      @command = "open #{parsed_command}"
+    end
+
+    def run
+      system(command)
+    end
+
+    private
+
+      def convert_ssh_to_http(git_remote_output)
+        git_remote_output.match(/origin\t(.+)\.git/)[1].gsub("com:", "com/").gsub("git@", "https://")
+      end
+  end
 end
--- test/brah_test.rb
+++ test/brah_test.rb
@@ -1,11 +1,69 @@
 require 'test_helper'
 
 class BrahTest < Minitest::Test
+
+  PUBLIC_URL = "https://github.com/trayo/brah"
+  PRIVATE_URL = "https://github.enterprise.com/trayo/brah"
+
   def test_that_it_has_a_version_number
     refute_nil ::Brah::VERSION
   end
 
-  def test_it_does_something_useful
-    assert true
+  def test_it_can_open_a_public_github_http_link
+    git_remote_http_public = "origin\thttps://github.com/trayo/brah.git (fetch)"
+
+    brah = Brah::Brah.new(git_remote_http_public)
+    brah.expects(:system).with("open #{PUBLIC_URL}").returns(true).once
+
+    assert_equal brah.command, "open https://github.com/trayo/brah"
+    assert brah.run, "Expected brah.run to be true, but it was false"
+  end
+
+  def test_it_can_open_a_public_github_ssh_link
+    git_remote_ssh_public = "origin\tgit@github.com:trayo/brah.git (fetch)"
+
+    brah = Brah::Brah.new(git_remote_ssh_public)
+    brah.expects(:system).with("open #{PUBLIC_URL}").returns(true).once
+
+    assert_equal brah.command, "open https://github.com/trayo/brah"
+    assert brah.run, "Expected brah.run to be true, but it was false"
+  end
+
+  def test_it_can_open_a_private_github_http_link
+    git_remote_http_private = "origin\thttps://github.enterprise.com/trayo/brah.git (fetch)"
+
+    brah = Brah::Brah.new(git_remote_http_private)
+    brah.expects(:system).with("open #{PRIVATE_URL}").returns(true).once
+
+    assert_equal brah.command, "open https://github.enterprise.com/trayo/brah"
+    assert brah.run, "Expected brah.run to be true, but it was false"
+  end
+
+  def test_it_can_open_a_public_github_ssh_link
+    git_remote_ssh_private = "origin\tgit@github.enterprise.com/trayo/brah.git (fetch)"
+
+    brah = Brah::Brah.new(git_remote_ssh_private)
+    brah.expects(:system).with("open #{PRIVATE_URL}").returns(true).once
+
+    assert_equal brah.command, "open https://github.enterprise.com/trayo/brah"
+    assert brah.run, "Expected brah.run to be true, but it was false"
+  end
+
+  def test_when_there_are_remotes_other_than_origin
+    git_remote_with_more_than_origin = "heroku\thttps://git.heroku.com/trayo/brah.git (fetch)\nheroku\thttps://git.heroku.com/trayo/brah.git (push)\norigin\thttps://github.com/trayo/brah.git (fetch)\norigin\thttps://github.com/trayo/brah.git (push)"
+
+    brah = Brah::Brah.new(git_remote_with_more_than_origin)
+    brah.expects(:system).with("open #{PUBLIC_URL}").returns(true).once
+
+    assert_equal brah.command, "open https://github.com/trayo/brah"
+    assert brah.run, "Expected brah.run to be true, but it was false"
+  end
+
+  def test_when_there_are_no_remotes_found
+    no_remotes = ""
+
+    assert_raises do
+      Brah::Brah.new(no_remotes)
+    end
   end
 end
